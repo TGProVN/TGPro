35c65eef20b9ddd83b394ec4713a6274bf3f7b01
Thuong Vo <hothy91@gmail.com>
2023-05-31 11:41:14 +0700

    add admin template

--- src/AdminPortal/Program.cs
+++ src/AdminPortal/Program.cs
@@ -4,10 +4,11 @@ using AdminPortal;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
