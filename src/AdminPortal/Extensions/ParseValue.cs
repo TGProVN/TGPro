0d4f5d736a4c1a961394db21569a178bc4f13d25
Thuong Vo <hothy91@gmail.com>
2023-06-07 12:43:04 +0700

    safe parse

---
+++ src/AdminPortal/Extensions/ParseValue.cs
@@ -0,0 +1,14 @@
+﻿namespace AdminPortal.Extensions;
+
+public static class ParseValue
+{
+    public static int ToIntValue<T>(this T value)
+    {
+        return typeof(T) == typeof(int) ? Convert.ToInt32(value) : default;
+    }
+
+    public static string ToStringValue<T>(this T value)
+    {
+        return typeof(T) == typeof(string) ? Convert.ToString(value) ?? "0" : "0";
+    }
+}
